2ed305ab11e3be2d0de2247fbfe63bf61a0e1851
tanvir <tanvir101tk@gmail.com>
2022-09-15 14:46:37 +0600

    timer part fixed

--- script.js
+++ script.js
@@ -67,7 +67,10 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  // const timeTaken = (finishTime - startTime) / 1000;
+  const timeForTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round(timeForTaken)
+
 
   // show result modal
   resultModal.innerHTML = "";
@@ -132,7 +135,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeForSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.round(timeForSpent)
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
